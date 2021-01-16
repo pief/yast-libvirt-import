2c3a429a4118afd3339082a4bb0063e77e410764
Pieter Hollants <pieter@hollants.com>
2021-01-17 00:29:03 +0100

    Fix the Yast patching for network installs
    Turns out the previous method actually only worked with the full DVD
    image where /usr/share/YaST2/ resides in tmpfs. Now we use the y2update/
    directory in which we place the patched files where they will be picked
    up by Yast.

--- test/test.sh
+++ test/test.sh
@@ -6,15 +6,16 @@ ISO=~/Work/ISOs/openSUSE-Leap-15.2-DVD-x86_64.iso
 
 function cleanup() {
     [ -d /tmp/dud ] && rm -r /tmp/dud
-    [ -f libvirt-import.dud ] && rm libvirt-import.dud
+    [ -f libvirt-import-dud.tar.gz ] && rm libvirt-import-dud.tar.gz
     [[ "$(LANG=C virsh -c qemu:///system domstate yasttest 2>&1)" == "running" ]] &&  virsh -c qemu:///system destroy yasttest >/dev/null
     virsh -c qemu:///system dominfo yasttest >/dev/null 2>&1 && virsh -c qemu:///system undefine --nvram yasttest >/dev/null
 }
 trap cleanup EXIT
 
-mkdir -p /tmp/dud/usr/share/YaST2
-cp -a ../src/* /tmp/dud/usr/share/YaST2/
-mkdud --create libvirt-import.dud --dist leap15.2 /tmp/dud update.pre
+mkdir -p /tmp/dud/y2update/
+(cd ../src && find * -name *.rb -exec cp --parents {} /tmp/dud/y2update/ \;)
+cp ../src/update.pre /tmp/dud/
+mkdud --create libvirt-import-dud.tar.gz --name yast-libvirt-import --dist leap15.2 --format tar.gz /tmp/dud/y2update /tmp/dud/update.pre
 rm -r /tmp/dud
 
 sudo qemu-img snapshot yasttest.qcow2 -a pre_test
@@ -25,6 +26,6 @@ virt-install --connect qemu:///system \
              --location $ISO \
              --disk yasttest.qcow2 \
              --boot uefi \
-             --initrd-inject libvirt-import.dud \
+             --initrd-inject libvirt-import-dud.tar.gz \
              --initrd-inject autoinst.xml \
-             -x "dud=file:/libvirt-import.dud insecure=1"
+             -x "dud=file:/libvirt-import-dud.tar.gz"
